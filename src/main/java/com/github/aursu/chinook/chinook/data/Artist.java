9846d18c5659c3dac11f3177975faf50d73236d3
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-26 20:29:40 +0100

    Added data class Artist

--- src/main/java/com/github/aursu/chinook/chinook/data/Artist.java
+++ src/main/java/com/github/aursu/chinook/chinook/data/Artist.java
@@ -1,2 +1,14 @@
-package com.github.aursu.chinook.chinook.data;public class Artist {
+package com.github.aursu.chinook.chinook.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// https://projectlombok.org/features/constructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Artist {
+    private int id;
+    private String name;
 }
