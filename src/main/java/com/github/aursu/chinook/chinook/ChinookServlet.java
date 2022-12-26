cbd39c7c0b9f6c2f2796422c24cef0a60e8064b8
Alexander Ursu <alexander.ursu@gmail.com>
2022-12-26 22:03:02 +0100

    Implemented showArtists method

--- src/main/java/com/github/aursu/chinook/chinook/ChinookServlet.java
+++ src/main/java/com/github/aursu/chinook/chinook/ChinookServlet.java
@@ -1,20 +1,35 @@
 package com.github.aursu.chinook.chinook;
 
+import com.github.aursu.chinook.chinook.beans.ArtistBean;
+import com.github.aursu.chinook.chinook.dao.ArtistDao;
+import com.github.aursu.chinook.chinook.data.Artist;
+
 import java.io.*;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.sql.DataSource;
 
 @WebServlet(name = "chinookServlet", value = "/artists")
 public class ChinookServlet extends HttpServlet {
-    public void init() {
+    @Resource(name = "jdbc/Chinook")
+    private DataSource ds;
 
+    private ArtistDao artistDao;
+
+    public void init() {
+        artistDao = new ArtistDao(ds);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         showArtists(request, response);
     }
 
-    private void showArtists(HttpServletRequest request, HttpServletResponse response) {
-
+    private void showArtists(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Artist> artists = artistDao.findAll();
+        request.setAttribute("artistBean", new ArtistBean(artists));
+        request.getRequestDispatcher("/artists.jsp").forward(request, response);
     }
 }
